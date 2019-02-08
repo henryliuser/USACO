ced16177d7fa2863bfa0bbb7bc16fb29a40f09c1
henry <henryliuser@gmail.com>
2019-02-07 22:33:36 -0800

    2/7 usaco grind begins

--- Bronze/sleepy.java
+++ Bronze/sleepy.java
@@ -1,30 +1,33 @@
-import java.util.Random;
 import java.util.Scanner;
 import java.io.*;
-
 public class sleepy {
-
-    static boolean debug = false;
     public static void main(String[] args) throws IOException {
-
-        String inpath = "Text Files/sleepy.in";
-        if (debug) inpath = "/Users/henry/Desktop/projects/USACO/Bronze/sleepy.in";
+        boolean debug = true;
+        String inpath = "sleepy.in";
+        if (debug) inpath = "/Users/henry/Desktop/projects/USACO/TextFiles/sleepy.in";
         Scanner in = new Scanner(new File(inpath));
-
         int N = in.nextInt();
-        int[] stuff = new int[N];
-
+        int[] grid = new int[N];
         for (int z = 0; z < N; z++) {
-            stuff[z] = in.nextInt();
+            grid[z] = in.nextInt();
         }
         in.close();
-
-        Random rand = new Random();
+        int temp = grid[N-1];
+        int x = 0;
+        for (int z = 1; z <= N; z++) {
+            int a = grid[N-z];
+            if (a > temp) {
+                x = z;
+                break;
+            }
+            else temp = a;
+        }
+        x +=1;
         BufferedWriter out = new BufferedWriter(new FileWriter("sleepy.out"));
-        if (N == 4) out.write((stuff.length-1) + "");
-        else out.write(rand.nextInt(N) + "");
+        System.out.println(x);
+        out.write((x) + "");
         out.close();
-
     }
-
 }
+
+
